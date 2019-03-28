3fd3e89032b159cb269d4f7ef16a18e8802316c6
Mike Marquez <mike.marquez11@gmail.com>
2019-03-28 16:25:12 -0600

    Creating command's functions and splitting input

--- main.js
+++ main.js
@@ -11,61 +11,118 @@ const rl = readline.createInterface({
 rl.prompt();
 
 rl.on('line', (line) => {
-  switch (line.trim()) {
+  let args = line.split(' ');
+  let cmd = args[0].toLowerCase().trim();
+
+  switch (cmd) {
     case '':
+      rl.prompt();
       break;
     case 'help':
     case 'h':
-      console.log('Comandos: ');
-      console.log(' h|help - Muestra esta ayuda');
-      console.log(' list - Listar los quizzes existentes');
-      console.log(' show <id> - Muestra la pregunta y la respuesta el quiz indicado');
-      console.log(' add - Anadir un nuevo quiz interactivamente');
-      console.log(' delete <id> - Borrar el quiz indicado');
-      console.log(' edit <id> - Editar el quiz indicado');
-      console.log(' test <id> - Probar el quiz indicado');
-      console.log(' p|play - Jugar a preguntar aleatoriamente todos los quizzes');
-      console.log(' credits - Creditos');
-      console.log(' q|quit - Salir del programa');
+      helpCmd();
       break;
     case 'quit':
     case 'q':
-      rl.close();
+      quitCmd();
       break;
     case 'add':
-      console.log('Anadir un nuevo quiz');
+      addCmd();
       break;
     case 'list':
-      console.log('Listar todos los quizzes existentes');
+      listCmd();
       break;
     case 'show':
-      console.log('Mostrar el quiz indicado');
+      showCmd(args[1]);
       break;
     case 'test':
-      console.log('Probar el quiz indicado');
+      testCmd(args[1]);
       break;
     case 'play':
     case 'p':
-      console.log('Jugar');
+      playCmd();
       break;
     case 'delete':
-      console.log('Borrar el quiz indicado');
+      deleteCmd(args[1]);
       break;
     case 'edit':
-      console.log('Editar el quiz indicado');
+      editCmd(args[1]);
       break;
     case 'credits':
-      console.log('Autores de la practica');
-      console.log('Miguel Angel Marquez Leon');
+      creditsCmd();
       break;
 
     default:
-      console.log(`Comando desconocido: '${line.trim()}'`);
+      console.log(`Comando desconocido: '${cmd}'`);
       console.log(`Use 'help' para ver todos los comandos disponibles`);
+      r1.prompt();
       break;
   }
-  rl.prompt();
+
 }).on('close', () => {
   console.log('Adios!');
   process.exit(0);
 });
+
+/**
+* It shows help
+*/
+const helpCmd = () => {
+  console.log('Comandos: ');
+  console.log(' h|help - Muestra esta ayuda');
+  console.log(' list - Listar los quizzes existentes');
+  console.log(' show <id> - Muestra la pregunta y la respuesta el quiz indicado');
+  console.log(' add - Anadir un nuevo quiz interactivamente');
+  console.log(' delete <id> - Borrar el quiz indicado');
+  console.log(' edit <id> - Editar el quiz indicado');
+  console.log(' test <id> - Probar el quiz indicado');
+  console.log(' p|play - Jugar a preguntar aleatoriamente todos los quizzes');
+  console.log(' credits - Creditos');
+  console.log(' q|quit - Salir del programa');
+  rl.prompt();
+};
+
+const listCmd = () => {
+  console.log('Listar todos los quizzes existentes');
+  rl.prompt();
+}
+
+const showCmd = (id) => {
+  console.log('Mostrar el quiz indicado');
+  r1.prompt();
+}
+
+const addCmd = () => {
+  console.log('Anadir un nuevo quiz');
+  rl.prompt();
+}
+
+const deleteCmd = (id) => {
+  console.log('Borrar el quiz indicado', id);
+  rl.prompt();
+}
+
+const editCmd = (id) => {
+  console.log('Editar el quiz indicado');
+  rl.prompt();
+}
+
+const creditsCmd = () => {
+  console.log('Autores de la practica');
+  console.log('Miguel Angel Marquez Leon');
+  rl.prompt();
+}
+
+const playCmd = () => {
+  console.log('Jugar');
+  rl.prompt();
+}
+
+const testCmd = (id) => {
+  console.log('Probar el quiz indicado');
+  rl.prompt();
+}
+
+const quitCmd = () => {
+  rl.close();
+}
