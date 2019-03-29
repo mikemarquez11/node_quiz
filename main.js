74795ec77de82c873484713c45b12e31a6f4f345
Mike Marquez <mike.marquez11@gmail.com>
2019-03-29 11:05:25 -0600

    Creating output module (log, biglog, errorlog, colorize Functions)

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
 const readline = require('readline');
-const figlet = require('figlet');
-const chalk = require('chalk');
+const model = require('./model');
 
-const model = require('./model')
-
-const colorize = (msg, color) => {
-  if (typeof color !== "undefined"){
-    msg = chalk[color].bold(msg);
-  }
-  return msg;
-};
-
-const log = (msg, color) => {
-  console.log(colorize(msg, color));
-};
-
-const bigLog = (msg, color) => {
-  log(figlet.textSync(msg, { horizontalLayout: 'full' }), color);
-}
-
-const errorLog = (emsg) => {
-  console.log(`${colorize('Error', 'red')}: ${colorize(colorize(emsg, 'red'), 'bgYellowBright')}`);
-}
-
-
+const {log, bigLog, errorLog, colorize} = require('./output');
 
 //Mensaje Inicial
 bigLog('Node Quiz','green');
---
+++ output.js
@@ -0,0 +1,28 @@
+const figlet = require('figlet');
+const chalk = require('chalk');
+
+const colorize = (msg, color) => {
+  if (typeof color !== "undefined"){
+    msg = chalk[color].bold(msg);
+  }
+  return msg;
+};
+
+const log = (msg, color) => {
+  console.log(colorize(msg, color));
+};
+
+const bigLog = (msg, color) => {
+  log(figlet.textSync(msg, { horizontalLayout: 'full' }), color);
+}
+
+const errorLog = (emsg) => {
+  console.log(`${colorize('Error', 'red')}: ${colorize(colorize(emsg, 'red'), 'bgYellowBright')}`);
+}
+
+exports = module.exports {
+  colorize,
+  log,
+  bigLog,
+  errorLog
+}
